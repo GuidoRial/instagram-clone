d731a297ba27bc5754d7169fbd9508553d1f231e
GuidoRial <rialguido@gmail.com>
2022-02-03 19:15:18 -0300

    fixed header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,31 @@ import React from "react";
 function Header() {
     return (
         <div className="header">
-            <div className="img-container">
-                <img
-                    src="https://logos-marcas.com/wp-content/uploads/2020/04/Instagram-Logo.png"
-                    alt="instagram-logo"
-                    className="instagram-logo-text"
-                />
-            </div>
-            <div className="search-bar-container">
-                <i className="fas fa-search search-icon" />
-                <input type="search" className="search-bar" />
-            </div>
-            <div className="icon-container">
-                <i className="fas fa-home navbar-icons" />
-                <i className="fas fa-inbox navbar-icons">
-                    <div className="amount-of-unread-messages">9+</div>
-                </i>
-                <i className="fas fa-plus-circle navbar-icons" />
-                <i className="fas fa-heart navbar-icons" />
-                <i className="fas fa-user-alt navbar-icons" />
+            <div className="header-content">
+                <div className="img-container">
+                    <img
+                        src="https://logos-marcas.com/wp-content/uploads/2020/04/Instagram-Logo.png"
+                        alt="instagram-logo"
+                        className="instagram-logo-text"
+                    />
+                </div>
+                <div className="search-bar-container">
+                    <i className="fas fa-search search-icon" />
+                    <input
+                        type="search"
+                        className="search-bar"
+                        placeholder="Search"
+                    />
+                </div>
+                <div className="icon-container">
+                    <i className="fas fa-home navbar-icons" />
+                    <i className="fas fa-inbox navbar-icons">
+                        <div className="amount-of-unread-messages">9+</div>
+                    </i>
+                    <i className="fas fa-plus-circle navbar-icons" />
+                    <i className="fas fa-heart navbar-icons" />
+                    <i className="fas fa-user-alt navbar-icons" />
+                </div>
             </div>
         </div>
     );
