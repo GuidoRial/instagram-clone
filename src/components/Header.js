26d24353ebcd01a4fd6c366df3d9a8d578f8714e
GuidoRial <rialguido@gmail.com>
2022-02-03 18:27:09 -0300

    added basic styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,26 @@ import React from "react";
 
 function Header() {
     return (
-        <div>
-            <div>
+        <div className="header">
+            <div className="img-container">
                 <img
                     src="https://logos-marcas.com/wp-content/uploads/2020/04/Instagram-Logo.png"
                     alt="instagram-logo"
+                    className="instagram-logo-text"
                 />
             </div>
-            <div>
-                <input type="search" />
+            <div className="search-bar-container">
+                <i className="fas fa-search search-icon" />
+                <input type="search" className="search-bar" />
             </div>
-            <div>
-                <i className="fas fa-home" />
-                <i className="fas fa-inbox" />
-                <i className="fas fa-plus-circle" />
-                <i className="fas fa-heart" />
-                <i className="fas fa-user-alt" />
+            <div className="icon-container">
+                <i className="fas fa-home navbar-icons" />
+                <i className="fas fa-inbox navbar-icons">
+                    <div className="amount-of-unread-messages">9+</div>
+                </i>
+                <i className="fas fa-plus-circle navbar-icons" />
+                <i className="fas fa-heart navbar-icons" />
+                <i className="fas fa-user-alt navbar-icons" />
             </div>
         </div>
     );
