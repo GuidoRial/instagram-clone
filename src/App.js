bb06feec4b29e77dddc331a3dc8e773bbdd046f1
GuidoRial <rialguido@gmail.com>
2022-02-04 19:09:25 -0300

    implemented signup page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Feed from "./components/Feed";
 import Header from "./components/Header";
+import SignUp from "./components/SignUp";
 
 function App() {
     return (
         <div className="App">
+            {/*<SignUp/>*/}
             <Header />
             <Feed />
         </div>
---
+++ src/components/SignUp.js
@@ -0,0 +1,30 @@
+import React from "react";
+import AppStoreLogo from "../assets/download-appStore.png";
+import GooglePlayLogo from "../assets/get-it-on-GooglePlay.png";
+
+function SignUp() {
+    return (
+        <div className="sign-up-container">
+            <div className="sign-up">
+                <img
+                    src="https://logos-marcas.com/wp-content/uploads/2020/04/Instagram-Logo.png"
+                    alt="instagram-logo"
+                    className="instagram-logo-text"
+                />
+                <div className="log-in-google">
+                    <i class="fab fa-google" />
+                    <p>Log in with Google</p>
+                </div>
+            </div>
+            <div>
+                <p>Get the app.</p>
+                <div>
+                    <img src={AppStoreLogo} alt="get-it-on-appstore"  className="log-in-icon"/>
+                    <img src={GooglePlayLogo} alt="get-it-on-play-store" className="log-in-icon" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignUp;
